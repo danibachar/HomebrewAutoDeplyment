ae9c5fbce027288158b8c22654948e2309ea5cfe
danibachar <some@hotmail.com>
2023-11-29 17:40:26 +0000

    New Release 0.2.0

---
+++ tuistenv@0.2.0.rb
@@ -0,0 +1,25 @@
+class TuistenvAt020 < Formula
+    desc "Managing Tuist versions"
+    homepage "https://tuist.io"
+    url "https://github.com/danibachar/WorkflowTagTests/archive/refs/tags/0.2.0.tar.gz"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    license "MIT"
+    head "https://github.com/tuist/tuist.git", branch: "main"
+  
+    # https://github.com/tuist/tuist/blob/3.33.4/.xcode-version#L1
+    depends_on xcode: ["14.3", :build]
+  
+    # https://github.com/tuist/tuist/blob/3.33.4/Package.swift#L14
+    depends_on macos: :monterey
+  
+    def install
+      # https://github.com/tuist/tuist/blob/3.33.4/.github/workflows/tuist.yml#L83
+      system "swift", "build",
+        "--configuration", "release",
+        "--product", "tuistenv",
+        "--disable-sandbox"
+  
+      bin.install ".build/release/tuistenv"
+    end
+  end
+  
